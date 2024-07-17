27a9844682cb0e2bffe081f559f4804d15ce2167
Dario Alvarez <vidaalhe90@gmail.com>
2024-07-16 23:39:25 -0600

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Chapter07/Examples/ex0701_analyze_numbers.c (first example of chapter 7)

--- Chapter07/Examples/ex0701_analyze_numbers.c
+++ Chapter07/Examples/ex0701_analyze_numbers.c
@@ -1,8 +1,11 @@
 
 /*
  * 7.3 - Case Study: Analyzing Numbers.
- * The problem is to write a  program that finds the number of items
- * above the average of all items.
+ * The problem is to write a program that:
+ * - Reads an integer N > 0 from console,
+ * - Enter N floating point numbers from console and store them in an array.
+ * - Computes the average of entered numbers
+ * - Count the number of items in array above the average computed.
  */
 
 #include <stdio.h>
@@ -10,21 +13,34 @@
 
 int main(int argc, char* argv[])
 {
+    /* Enter an integer N > 0 */
     int n = 0;
     printf("Enter the number of items: ");
     scanf("%d", &n);
 
-    double* numbers = (double*)calloc(n, sizeof (double));
-    double sum = 0.0;
+    if (n <= 0)
+    {
+        printf("%d is an invalid value\n", n);
+        return (EXIT_FAILURE);
+    }
 
+    /* Enter N floating point numbers. */
+    double* numbers = (double*)calloc(n, sizeof (double));
     printf("Enter the numbers: ");
     for (int i = 0; i < n; i++)
     {
         scanf("%lf", &numbers[i]);
+    }
+
+    /* Computes the average of entered values. */
+    double sum = 0.0;
+    for (int i = 0; i < n; i++)
+    {
         sum += numbers[i];
     }
     double average = sum / n;
 
+    /* Count items above the average. */
     int count = 0;
     for (int i = 0; i < n; i++)
     {
@@ -34,7 +50,9 @@ int main(int argc, char* argv[])
         }
     }
 
-    printf("Average is: %.3lf\n", average);
+    /* Show results */
+    printf("Average is: %.2lf\n", average);
     printf("Number of elements above average: %d\n", count);
+
     return (EXIT_SUCCESS);
 }
